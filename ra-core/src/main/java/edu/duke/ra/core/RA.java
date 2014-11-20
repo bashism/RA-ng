4db3ea30cd1159ea42032712ace1922935e5d815
Michael Ren <micron33@gmail.com>
2014-11-20 17:27:44 -0500

    add query parsing, command tree generation, and SQL query generation

---
+++ ra-core/src/main/java/edu/duke/ra/core/RA.java
@@ -0,0 +1,95 @@
+package edu.duke.ra.core;
+
+import antlr.CommonAST;
+import antlr.RecognitionException;
+import antlr.TokenStreamException;
+import antlr.collections.AST;
+
+import java.sql.*;
+import java.io.*;
+
+public class RA {
+    private RAConfig config;
+    private DB database;
+
+    public RA(RAConfig config, DB database){
+        this.config = config;
+        this.database = database;
+    }
+    public QueryResult query(String query){
+        AST queryAST = parseQuery(query);
+        RAXNode queryCommandTree = generateCommandTree(queryAST);
+        String querySQL = generateSQLQuery(queryCommandTree, this.database);
+
+        ResultSet querySQLResult = executeQuery(querySQL);
+        QueryResult queryResult = new QueryResult(querySQLResult);
+        return queryResult;
+    }
+    AST parseQuery(String query) {
+        Reader queryReader = new StringReader(query);
+        RALexer lexer = new RALexer(queryReader);
+        RAParser parser = new RAParser(lexer);
+        try {
+            parser.start();
+        }
+        //FIXME: throw this up
+        catch (RecognitionException | TokenStreamException e) {
+            System.out.println("FIXME: parser has an error recognizing the query");
+        }
+        AST ast = parser.getAST();
+        return ast;
+    }
+    RAXNode generateCommandTree(AST ast){
+        RAXConstructor constructor = new RAXConstructor();
+        RAXNode raxTree = null;
+        try {
+            raxTree = constructor.expr(ast);
+        }
+        //FIXME: throw this up
+        catch (RecognitionException e) {
+            System.out.println("FIXME: parser error");
+        }
+        return raxTree;
+        
+    }
+
+    String generateSQLQuery(RAXNode commandRoot, DB database) {
+    	StringBuilder querySQL =
+    			generateTempViewDefinitions(commandRoot, database, 0);
+        querySQL.append("SELECT * FROM " + commandRoot.getViewName() + ";");
+        return querySQL.toString();
+    }
+
+    StringBuilder generateTempViewDefinitions(RAXNode commandRoot, DB database, int depth){
+        StringBuilder queryBuilder = new StringBuilder();
+        for (int i = 0; i < commandRoot.getNumChildren(); i++) {
+            queryBuilder.append(generateTempViewDefinitions(commandRoot.getChild(i), database, depth + 1));
+        }
+        queryBuilder.append(generateWithStatement(commandRoot, database));
+        if (depth > 0) {
+            queryBuilder.append(",");
+        }
+        queryBuilder.append("\n");
+        return queryBuilder;
+    }
+    StringBuilder generateWithStatement(RAXNode node, DB database) {
+        StringBuilder withStatementBuilder = new StringBuilder();
+        withStatementBuilder.append("WITH ");
+        withStatementBuilder.append(node.getViewName());
+        withStatementBuilder.append(" AS (");
+        try {
+            withStatementBuilder.append(node.genViewDef(database));
+        }
+        //FIXME
+        catch (SQLException | ValidateException e) {
+            System.out.println("FIXME: SQL error");
+        }
+        withStatementBuilder.append(")");
+        return withStatementBuilder;
+    }
+
+    //TODO
+    ResultSet executeQuery(String query){
+        return null;
+    }
+}
---
+++ ra-core/src/test/java/edu/duke/ra/core/RATest.java
@@ -0,0 +1,110 @@
+package edu.duke.ra.core;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import antlr.CommonAST;
+import antlr.collections.AST;
+
+public class RATest {
+    private DB database;
+    private RA ra;
+
+    @Before
+    public void setupDatabase() throws IOException, SQLException{
+        Properties properties = new Properties();
+		properties.load(this.getClass().getResourceAsStream("/ra.properties"));
+        this.database = new DB(properties.getProperty("url"), properties);
+        this.ra = new RA(new RAConfig(), database);
+    }
+    /**
+     * Run a simple, nested query:
+     * \\project_{name} (\\select_{name = 'Corona'} beer)
+     * 
+     *  Should generate this AST:
+     *  \project
+     *  +--name
+     *  +--\select
+     *     +--name = 'Corona'
+     *     +--beer
+     */
+    @Test
+    public void testParseQuery(){
+        //product placement
+        String query = "\\project_{name} \\select_{name = 'Corona'} beer;\n";
+
+        AST parseQueryResult = ra.parseQuery(query);
+        assertEquals("\\project", parseQueryResult.getText());
+        assertEquals(2, parseQueryResult.getNumberOfChildren());
+
+        AST projectPredicate = parseQueryResult.getFirstChild();
+        assertEquals("name", projectPredicate.getText());
+        assertEquals(0, projectPredicate.getNumberOfChildren());
+
+        AST selectOperator = parseQueryResult.getFirstChild().getNextSibling();
+        assertEquals("\\select", selectOperator.getText());
+        assertEquals(2, selectOperator.getNumberOfChildren());
+
+        AST selectPredicate = selectOperator.getFirstChild();
+        assertEquals("name = 'Corona'", selectPredicate.getText());
+        assertEquals(0, selectPredicate.getNumberOfChildren());
+
+        AST selectRelation = selectPredicate.getNextSibling();
+        assertEquals("beer", selectRelation.getText());
+        assertEquals(0, selectRelation.getNumberOfChildren());
+    }
+    /**
+     * Run a simple, nested query:
+     * \\project_{name} (\\select_{name = 'Corona'} beer)
+     * 
+     * After generating the AST, generate the command tree, which will look like:
+     * RAXNode.PROJECT
+     * +--RAXNode.SELECT
+     *    +--RAXNode.TABLE
+     */
+    @Test
+    public void testGenerateCommandTree() throws SQLException, ValidateException{
+        String query = "\\project_{name} \\select_{name = 'Corona'} beer;\n";
+        AST parseQueryResult = ra.parseQuery(query);
+
+        RAXNode root = ra.generateCommandTree(parseQueryResult);
+        assertEquals("PROJECT", root.getClass().getSimpleName());
+        assertEquals(1, root.getNumChildren());
+
+        RAXNode selectNode = root.getChild(0);
+        assertEquals("SELECT", selectNode.getClass().getSimpleName());
+        assertEquals(1, selectNode.getNumChildren());
+
+        RAXNode tableNode = selectNode.getChild(0);
+        assertEquals("TABLE",tableNode.getClass().getSimpleName());
+        assertEquals(0, tableNode.getNumChildren());
+    }
+    /**
+    * Run a simple, nested query:
+    * \\project_{name} (\\select_{name = 'Corona'} beer)
+    * 
+    * RA will generate temporary "view" names using WITH clauses;
+    * these will be appended to the beginning of the eventual SQL query;
+    * the actual query itself will be those view definitions plus a select *
+    * from the "view" definition of the root
+    */
+    @Test
+    public void testGenerateSQLQuery(){
+        String query = "\\project_{name} \\select_{name = 'Corona'} beer;\n";
+        AST parseQueryResult = ra.parseQuery(query);
+        RAXNode root = ra.generateCommandTree(parseQueryResult);
+        String SqlQuery = ra.generateSQLQuery(root, database);
+        String queryExpected = "WITH RA_TMP_VIEW_1 AS (SELECT DISTINCT * FROM beer),\n"
+        		+ "WITH RA_TMP_VIEW_2 AS (SELECT * FROM RA_TMP_VIEW_1 WHERE name = 'Corona'),\n"
+        		+ "WITH RA_TMP_VIEW_3 AS (SELECT DISTINCT name FROM RA_TMP_VIEW_2)\n"
+        		+ "SELECT * FROM RA_TMP_VIEW_3;";
+        assertEquals(queryExpected, SqlQuery);
+    }
+}
