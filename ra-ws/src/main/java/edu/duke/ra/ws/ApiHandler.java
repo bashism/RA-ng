08c92e4bf34e0242956eb59e8b2e1e8a4059e1e1
Michael Ren <micron33@gmail.com>
2014-12-08 10:15:11 -0500

    implement basic REST query call

---
+++ ra-ws/src/main/java/edu/duke/ra/ws/ApiHandler.java
@@ -0,0 +1,36 @@
+package edu.duke.ra.ws;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import edu.duke.ra.core.RA;
+import edu.duke.ra.core.RAConfig;
+import edu.duke.ra.core.RAConfigException;
+
+/**
+ * The request handler for the RA web API
+ */
+@Path("api")
+public class ApiHandler {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String query(@QueryParam("query") String queryString) {
+        try {
+            RAConfig config = new RAConfig.Builder().url("jdbc:sqlite://sample.db").build();
+            RA ra = new RA(config);
+            String fullQueryString = queryString + ";\n";
+            String result = ra.query(fullQueryString).toJsonString();
+            ra.closeDBConnection();
+            return result;
+        } catch (IOException | SQLException | RAConfigException e) {
+            return e.getMessage();
+        }
+    }
+}
